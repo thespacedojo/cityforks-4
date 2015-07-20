dfb8b3bc9853ae1d8c3dbaa2c767ee27ec4857c6
Josh Owens <joshua.owens@gmail.com>
2015-07-20 09:41:40 -0700

    Get location working.

---
+++ client/location.js
@@ -0,0 +1,7 @@
+Meteor.startup(function() {
+  navigator.geolocation.getCurrentPosition(success);
+});
+
+success = function(position) {
+  Session.set('location', {latitude: position.coords.latitude, longitude: position.coords.longitude});
+};
