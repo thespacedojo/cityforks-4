9b8550835926ac87d95ff3a4e6b1c8a176ef7d3a
Amanda Saffer <asaffer@sosorgans.org>
2015-11-16 19:43:04 -0600

    rewrite getStars function

--- client/helpers.js
+++ client/helpers.js
@@ -1,20 +1,29 @@
-Template.registerHelper('getStars', function(rating){
+Template.registerHelper('getStars', function(rating) {
 	var str = '';
-	var num = Math.round(rating * 2)/2; // nearest .5 rating
-	var rating = Math.round(rating);
-	var diff = 5 - rating;
+	var rating = rating.toString();
+	var substr = rating.split('.');
+	var decimal = substr[1];
 
-	for(i = 0; i < num; i++) {
+	// put full stars for whole number before the decimal
+	for(i = 0; i < substr[0]; i++) {
 		str += '<i class="icon ion-ios-star"></i>';
+		count++;
 	}
 
-	if ( rating - num != 0 ) {
-		str = str.substring(0, str.length - 33);
-		str += '<i class="icon ion-ios-star-half"></i>';
+	// determine which star (full, half, empty) depending on decimal
+	if (decimal) {
+		if(decimal <= 3) {
+			str += '<i class="icon ion-ios-star-outline"></i>';
+		} else if (decimal >= 4 && decimal < 8) {
+			str += '<i class="icon ion-ios-star-half"></i>';
+		} else {
+			str += '<i class="icon ion-ios-star"></i>';
+		}
 	}
 
-
-	for(j = 0; j < diff; j++) {
+	// if there are less than 5 stars, fill out the stars up to 5
+	var count = (str.match(/<\/i>/g)).length;
+	for(j = count; j < 5; j++) {
 		str += '<i class="icon ion-ios-star-outline"></i>';
 	}
 
